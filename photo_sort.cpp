f1046e120053c787460e45a02350176268539464
Jeremy-Marbles <marblekid15@gmail.com>
2025-05-16 21:47:47 -0700

    added error handling to constructors

--- photo_sort.cpp
+++ photo_sort.cpp
@@ -2,13 +2,23 @@
 
 namespace photo {
 
+    // Constructors
     photoSorter::photoSorter() {
-	//explicitly call for windows at start
-	root_directory = "C:";
-	num_files = 0;
-	file_position = nullptr;
-	file_path = "";
-	swap = false;
+        root_directory_{};
+        num_files = 0;
+        file_position = nullptr;
+        file_path = "";
+        swap = false;
+
+	std::error_code constructEC;
+	const auto currentPath = std::filesystem::current_path(constructEC);	//current_path() handles search, returns a _win_error if path cannot be found;
+
+	if (constructEC) {
+		throw std::filesystem::filesystem_error("Failed to get current working directory.", ec);
+	};
+
+	root_directory_ = currentPath;
+
     }   
 
     explicit photoSorter::photoSorter(const std::filesystem::path& root_directory) {
@@ -17,18 +27,51 @@ namespace photo {
         file_position = nullptr;
         file_path = "";
         swap = false;
+	if (root_directory_.empty()) {
+		throw std::invalid_argument("Root directory path cannot be empty.");
+	}
+
+	std::error_code variableConstructEC;
+	bool existingPath = std::filesystem::exists(root_directory_, ec);
+	if (variableConstructEC) {
+		throw std::filesystem::filesystem_error(
+			"Error verifying existence of specified root directory: " + root_directory_.string(), root_directory_, variableConstructEC
+		);
+	}
+
+	if (!path_exists) {
+		throw std::filesystem::filesystem_error ("Specified root directory does not exist: " + root_directory_.string(), root_directory_, std::make_error_code(std::errc::no_such_file_or_directory));
+	}
+	
+	bool isDir = std::filesystem::is_directory(root_directory_, variableConstructEC);
+	if (variableConstructEC) {
+		throw std::filesystem::filesystem_error(
+			"Error checking specified root path is a directory: " + root_directory_.string(), root_directory_, variableConstructEC
+		);
+	}
+
+	if(!isDir) {
+		throw std::filesystem::filesystem_error(
+			"Specified root path is not a directory: " + root_directory_.string(), root_directory_, std::make_error_code(std::errc::not_a_directory));
+	}
     }
     
+    // Destructor
     photoSorter::~photoSorter() {
-
+      	delete file_position;
     }
 
+    // Getters
     std::string photoSorter::getFileName(const std::filesystem::path& fileName) {
         return fileName.filename().string();
 	
     }
 
     std::string photoSorter::getFilePath(const std::filesystem::path& pathName) {
+        	
+    }
+
+    std::uint32_t photoSorter::getNumFiles() {
 	
     }
 }
--- photo_sort.h
+++ photo_sort.h
@@ -4,6 +4,8 @@
 #include <atomic>
 #include <cstdint>
 #include <filesystem>
+#include <system_error>
+#include <stdexcept>
 #include <thread>
 #include <mutex>
 
@@ -15,15 +17,19 @@ namespace photo {
 			~photoSorter(); 
 
 			//operating system constructor needed?
+			//Not needed: std::filesystem:path handles path formats 
 
 			std::string getFileName(const std::filesystem::path& fileName);
 			std::string getFilePath(const std::filesystem::path& pathName);
 
 			std::uint32_t getNumFiles();
+
+			//TODO: add mutator functions to set the root directory, current working directory and file name
 			
 			
 		private:
 			std::filesystem::path root_directory_;
+			std::filesystem::path current_working_directory_;
 			std::string file_name;
 			std::atomic<std::uint32_t> num_files;	//number of files in a folder structure, directories also count as files
 			int *file_position;	//thread specific file position
