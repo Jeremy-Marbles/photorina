360449f7277923eebf7b865920614f321d7e87f1
Jeremy-Marbles <marblekid15@gmail.com>
2026-04-11 13:26:37 -0700

    base actions complete

--- program-draft.cpp
+++ program-draft.cpp
@@ -17,7 +17,48 @@
  * -sc -> setCWD, takes 1 string path.
  * */
 
-int draftmain_WIN(int __argc, char** __argv) {
+//helper functions
+struct ParseCommand {
+    std::string flag;
+    std::vector<std::string> args;
+};
+
+void printHelp(const char* programName) {
+    std::cout << "Photorina - Photo Sorting and Organization Tool \n\n";
+    std::cout << "Usage: " << programName << " [OPTIONS]\n\n";
+    
+    std::cout << "Examples:\n";
+    std::cout << " " << programName << " -sd \"C:\\Photos\\Sorted\"\n";
+}
+
+std::vector<ParseCommand> parseArguments(int argc, char** argv) {
+    std::vector<ParseCommand> commands;
+
+    for (int i = 1; i < argc; ) {
+        std::string arg = argv[i];
+        ParseCommand cmd;
+        cmd.flag = arg;
+
+        if (arg == "-h" || arg == "--help") {
+            printHelp(argv[0]);
+            std::exit(0);
+        }
+        else if (arg == "-sd") {
+            if (i + i >= argc) {
+                std::cerr << "Error: -sd requires a path argument" << std::endl;
+            }
+        }
+    }
+}
+
+int draftmain_WIN(int argc, char** argv) {
+    if (argc == 1) {
+        std::cout << "Usage:" <<  argv[0] << " [OPTION] <path>" << std::endl;
+        std::cerr << argv[0] << ": error: you must provide a command and path to your folder" << std::endl;
+        std::cout << "Type " << argv[0] << " -h or " << argv[0] << " --help to see a list of options and usage guide" << std::endl;
+        std::cout << "Note: first time run initiates a configuration set up. Please make sure to follow the instructions when prompted" << std::endl;
+        return 0;
+    }
 	bool settingFileValid = false;
     //const char* username = std::getenv("USERNAME");
     //std::filesystem::path config = "C" / "Users" / username / "appdata" / "locallow";
@@ -36,24 +77,37 @@ int draftmain_WIN(int __argc, char** __argv) {
     if (!settingFileValid) {
         //debug
         //std::cerr << "Settings file not found. Creating default settings.toml..." << std::endl;
-        std::cout << "First time configuration:" << std::endl;
+        std::cout << "Warning - First time configuration:" << std::endl;
         photo::photoSettings initPhotos;
         
+        for (std::filesystem::directory_iterator it(test); it != std::filesystem::directory_iterator(); ++it) {
+            if (!std::filesystem::exists(test / "settings.toml")) {
+                throw std::runtime_error("Error: Failed to create settings.toml. Please check your file permissions and try again.");
+            }
+        }
         settingFileValid = true;
+        std::cout << "'settings.toml' created in APPDATA/locallow/photorina" << std::endl;
+
     } 
+
     if (settingFileValid) {
         //debug
         //std::cout << "Verified settings.toml." << std::endl;
         photo::photoSort newSort;
-        
+               
         
     }
 
     return 0;
 }
 
-int draftmain_LINUX(int __argc, char** __argv) {
-    
+int draftmain_LINUX(int argc, char** argv) {
+    if (argc == 1) {
+        std::cout << "Usage: photorina [OPTION] <path>" << std::endl;
+        std::cerr << "photorina.exe: error: you must provide a command and path to your folder" << std::endl;
+        std::cout << "Type photorina -h or photorina --help to see a list of options and usage guide" << std::endl;
+        return 0;
+    }
 }
 
 
